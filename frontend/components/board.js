ecaa3ecf57b06df23172e6e73e04f82a70687b04
Juan Ramirez <ramirjua94@gmail.com>
2020-03-29 00:42:15 -0700

    Begin adding move functionality

--- frontend/components/board.js
+++ frontend/components/board.js
@@ -3,55 +3,104 @@ var React = require('react')
 const boardHeight = 10;
 const boardWidth = 10;
 
+const Direction = {"Up":1, "Down":2, "Left":3, "Right":4}
+Object.freeze(Direction)
+
 function Square(props) {
   return (
     <div className={props.value}>
+      {props.value}
     </div>
   );
 }
 
-function Ant()
-{
-  return (
-    <div className="white">
-      <img src="download.png" id="pic"/>
-    </div>
-  );
-}
-
-module.exports = React.createClass({
+Ant = React.createClass ({
   getInitialState: function() {
     return {
-      squares: Array(boardHeight*boardWidth).fill("white"),
+      direction: Direction.Up,
     }
   },
 
-  renderSquare: function(i) {
+  nextMove: function(props) {
+    if (this.isOnBlack(props))
+    {
+
+    }
+  },
+
+  isOnBlack: function(props) {
+    var squares = props.squares
+    var X = props.X
+    var Y = props.Y
+
+    var currentColor = squares[X][Y]
+
+    return currentColor === "black"
+  },
+
+  render: function() {
+    return (
+      <div className="white">
+        <input type="image" src="./download.png" id="pic" onClick={() => this.nextMove(this.props)} />
+      </div>
+    );
+  }
+});
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    var squares = new Array(boardWidth)
+    for (var i = 0; i < boardWidth; i++)
+    {
+      var inner = Array(boardHeight).fill("white")
+      squares[i] = inner
+    }
+
+    return {
+      squares: squares,
+      X: 0,
+      Y: 0,
+    }
+  },
+
+  renderSquare: function(x, y) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.state.squares[x][y]}
       />
     );
   },
 
   renderAnt: function() {
     return (
-      <Ant/>
+      <Ant
+      X={this.state.X}
+      Y={this.state.Y}
+      setNewBoardState={this.setNewBoardState}
+      squares={this.state.squares.slice()}
+      />
     );
+  },
+
+  setNewBoardState: function(x, y) {
+    const squares = this.state.squares.slice()
+    this.setState({
+      squares: squares,
+      X: x,
+      Y: y,
+    })
   },
 
   render: function() {
     var board = [];
-    var squareNum = 0;
 
     for(var i=0; i<boardHeight; i++) {
       var boardRow = [];
       for(var j=0; j<boardWidth; j++) {
-        if (i==0 && j==0)
+        if (i===this.state.X && j===this.state.Y)
           boardRow.push(this.renderAnt());
         else
-          boardRow.push(this.renderSquare(squareNum));
-        squareNum++;
+          boardRow.push(this.renderSquare(i, j));
       }
       board.push(<div className="board-row">{boardRow}</div>);
     }
